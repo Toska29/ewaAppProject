84494d4ab0052e79e1a4b5191ec077555e5efd16
Tosin Akinnusi <akinnusitosinbenedict@gmail.com>
2021-11-18 09:59:00 +0100

    Customer model

---
+++ src/main/java/africa/semicolon/ewaApp/data/models/Customer.java
@@ -0,0 +1,12 @@
+package africa.semicolon.ewaApp.data.models;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+}
